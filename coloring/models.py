48932b3295014e752a277dad045bf5c78cbdd0ad
peterhma <71454292+peterhma@users.noreply.github.com>
2022-04-14 17:50:41 +0000

    Test

--- coloring/models.py
+++ coloring/models.py
@@ -1,3 +1,5 @@
+
+# Create the Drawing model
 from django.db import models
 
 # The Author model has been created for you
@@ -5,4 +7,13 @@ class Author(models.Model):
   name = models.CharField(max_length=50)
 
 # Create the Drawing model
+class Drawing(models.Model):
+  title = models.CharField(max_length=50)
+  author = models.ForeignKey('Author', on_delete=models.CASCADE,)
+  drawingFile = models.JSONField()
+
+  
+
+
+
 
